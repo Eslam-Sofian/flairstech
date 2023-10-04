a723c9af40c6f97e3799ae777877624cf5cb47d0
Eslam-Sofian <alsofian92@gmail.com>
2023-10-04 20:11:59 +0300

    Enhance CartItemsPriceValidation test case

--- cypress/e2e/ShopingSuit.cy.js
+++ cypress/e2e/ShopingSuit.cy.js
@@ -4,40 +4,45 @@ import LoginPage from "../pages/LoginPage.js";
 describe('ShopingSuit', () => {
     const SP = new ShoppingPage();
     beforeEach(() => {
-        cy.visit('https://www.jumia.com.eg/',{failOnStatusCode: false})
+        cy.visit('https://www.jumia.com.eg/', { failOnStatusCode: false })
         const HP = new HomePage();
         const UL = new LoginPage();
         HP.ClosePopup()
         HP.RegisterNavigation()
         cy.fixture('data').as('data')
         cy.get('@data').then(input => {
-        UL.Login(input.Email,input.Password).contains(input.FirstName)})
+            UL.Login(input.Email, input.Password).contains(input.FirstName)
+        })
         HP.OpenSupermarket()
-        
+
     })
     afterEach(() => {
         SP.ClearCart()
         SP.Logout()
-      })
+    })
     it('AddToCartItemsValidation', () => {
         cy.fixture('data').as('data')
-       cy.get('@data').then(input => {
-           SP.AddToCart(input.ProductNo)
-       })
-       SP.AddMoreQuantity()
-       SP.CheckCartItemsNo().contains('2')
-        })  
-        
+        cy.get('@data').then(input => {
+            SP.AddToCart(input.ProductNo)
+        })
+        SP.AddMoreQuantity()
+        SP.CheckCartItemsNo().contains('2')
+    })
+
     it('CartItemsPriceValidation', () => {
         cy.fixture('data').as('data')
-       cy.get('@data').then(input => {
-           SP.AddToCart(input.ProductNo)
-       })
-       SP.AddMoreQuantity()
+        cy.get('@data').then(input => {
+            SP.AddToCart(input.ProductNo)
+        })
+        SP.AddMoreQuantity()
         SP.OpenCart()
-        SP.CompareItemAndTotal();
-            })  
-    
-            
-            
+        let itemNum;
+        SP.ItemPrice().then((itemPrice) => {
+            itemNum = itemPrice.replace('EGP ', '').trim();
+            console.log({ itemNum })
+            itemNum = (Number(itemNum) * 2).toFixed(2);
+            SP.TotalPrice().should('eq', itemNum);
+        });
+    })
+
 })
--- cypress/pages/ShoppingPage.js
+++ cypress/pages/ShoppingPage.js
@@ -1,40 +1,50 @@
-class ShoppingPage{
-    AddToCart (FProductNo){
-    cy.get("div[data-catalog='true']>article:nth-of-type("+FProductNo+')').click()
-    cy.get("form[id='add-to-cart']>button[type='button'][name='action']").click()
+class ShoppingPage {
+    AddToCart(FProductNo) {
+        cy.get("div[data-catalog='true']>article:nth-of-type(" + FProductNo + ')').click()
+        cy.get("form[id='add-to-cart']>button[type='button'][name='action']").click()
     }
-    AddMoreQuantity(){
+    AddMoreQuantity() {
         cy.get("button[type='button'][aria-label='increase cart quantity']").click()
     }
-    OpenCart(){
+    OpenCart() {
         cy.get('a.-df.-i-ctr.-gy9.-hov-or5.-phs').click()
     }
-    CheckCartItemsNo(){
+    CheckCartItemsNo() {
         cy.get('a.-df.-i-ctr.-gy9.-hov-or5.-phs').click()
-       return cy.get('span.-w-32')
+        return cy.get('span.-w-32')
     }
-    ClearCart(){
+    ClearCart() {
         cy.get("button[data-pop-open='removeFromCart']").click()
         cy.get('button.btn._prim._i.-fw.-fh').click()
     }
-   
-Logout(){
-    cy.get("div:nth-of-type(1)>label[for='dpdw-login']").click()
-    cy.get("button[class*='btn'][type='submit']").click()
-}
-CompareItemAndTotal(){
- 
-    cy.get("div[class='prc']:first-of-type")
-          .invoke('text')
-          .then((itemPrice) => {
-              const newEle = () => itemPrice.replace('EGP ', '').trim();
-              cy.get("article[class='card']>div[class*='-i-ctr']>p[class^='-fs20']")
-                  .invoke('text')
-                  .then((totalPrice) => totalPrice.replace('EGP ', '').trim())
-                   .should('eq',(Number(newEle())*2).toFixed(2));
-          });}
 
-          
-          
- }
- export default ShoppingPage
+    Logout() {
+        cy.get("div:nth-of-type(1)>label[for='dpdw-login']").click()
+        cy.get("button[class*='btn'][type='submit']").click()
+    }
+    CompareItemAndTotal() {
+
+        cy.get("div[class='prc']:first-of-type")
+            .invoke('text')
+            .then((itemPrice) => {
+                const newEle = () => itemPrice.replace('EGP ', '').trim();
+                cy.get("article[class='card']>div[class*='-i-ctr']>p[class^='-fs20']")
+                    .invoke('text')
+                    .then((totalPrice) => totalPrice.replace('EGP ', '').trim())
+                    .should('eq', (Number(newEle()) * 2).toFixed(2));
+            });
+    }
+
+    ItemPrice() {
+        return cy.get("div[class='prc']:first-of-type")
+            .invoke('text');
+    }
+    TotalPrice() {
+        return cy.get("article[class='card']>div[class*='-i-ctr']>p[class^='-fs20']")
+            .invoke('text')
+            .then((totalPrice) => totalPrice.replace('EGP ', '').trim())
+
+    }
+
+}
+export default ShoppingPage
